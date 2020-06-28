679bf9d52b6acb1a3025c2a768006a7f48a08f00
Roan <rschipman@gmail.com>
2020-06-27 21:25:04 -0600

    Added comments

--- designs.js
+++ designs.js
@@ -1,28 +1,28 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
 
 function makeGrid() {
-// Your code goes here!
+
     const canvas = document.querySelector("#pixelCanvas");
-    let height = document.querySelector("#inputHeight").value;
-    let width = document.querySelector("#inputWidth").value;
+    const height = document.querySelector("#inputHeight").value;
+    const width = document.querySelector("#inputWidth").value;
+    /*The empty string allows the grid to refresh before each new grid submission*/
     canvas.innerHTML = "";
-    for(var i = 0; i < height; i++){
+    for(let i = 0; i < height; i++){
+        /*This loop creates rows to match the input value */
         const rows = document.createElement("tr");
-        canvas.appendChild(rows);
-        for ( var j = 0; j < width; j++){
+        canvas.appendChild(rows); 
+        for (let j = 0; j < width; j++){
+            /*In this for loop we create the boxes for the colums and to each
+            we then attach an eventlistener that picks up the color selection*/
             const colums = document.createElement("td");
-            rows.appendChild(colums);
+            rows.appendChild(colums); 
             colums.addEventListener("click", function(){
-                let color = document.querySelector("#colorPicker").value;
+                const color = document.querySelector("#colorPicker").value;
                 colums.style.backgroundColor = color;
             })
         }
     }
 }
+/*Here we listen for the click on the submit button in order to start the makeGrid function*/
 const pickSize = document.querySelector("#submit");
 pickSize.addEventListener("click", function(event){
     event.preventDefault();
